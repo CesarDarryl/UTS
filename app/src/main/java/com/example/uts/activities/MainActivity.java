b9c37facfee598fd9f4551b99ae6768dee5109a2
Devany <devanydc>
2019-10-13 22:16:41 +0700

    SplashScreen has Done, Welcome Screen has Done (without content),Home has build

--- app/src/main/java/com/example/uts/activities/MainActivity.java
+++ app/src/main/java/com/example/uts/activities/MainActivity.java
@@ -1,16 +1,64 @@
 package com.example.uts.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.uts.R;
+import com.example.uts.fragments.MinumanFragment;
+import com.example.uts.fragments.PermenFragment;
+import com.example.uts.fragments.SnackFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new PermenFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
     }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new PermenFragment();
+                break;
+            case R.id.action_movie:
+                fragment = new SnackFragment();
+                break;
+            case R.id.action_discount:
+                fragment = new MinumanFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+
 }
